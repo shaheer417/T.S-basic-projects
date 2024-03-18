df20c04ca56fd1d520386967bd62ae353c70d74c
shaheer417 <117895824+shaheer417@users.noreply.github.com>
2024-03-18 23:39:40 +0500

    Add files via upload

---
+++ 04calculator.ts
@@ -0,0 +1,85 @@
+// import inquirer from "inquirer";
+
+// const input1 = await inquirer.prompt({
+//     name : "num1",
+//     type : "number",
+//     message : "Enter the number"
+// })
+
+// const input2 = await inquirer.prompt({
+//     name : "num2",
+//     type : "number",
+//     message : "Enter the second number"
+// })
+
+// const operator = await inquirer.prompt({
+//         type: 'list',
+//         name: 'operator',
+//         message: 'Select an operator:',
+//         choices: ['+', '-', '*', '/', '**', '%']
+// })     
+
+// let result : number;
+
+// if(operator.operator === '+'){
+//     result = input1.num1 + input2.num2;
+//     console.log(`Total : ${result}`);
+        
+// }
+// else if(operator.operator === '-'){
+//     result = input1.num1 - input2.num2;
+//     console.log(`Total : ${result}`);
+// }
+// else if(operator.operator === '*'){
+//     result = input1.num1 * input2.num2;
+//     console.log(`Total : ${result}`);
+// }
+// else if(operator.operator === '/'){
+//     result = input1.num1 / input2.num2;
+//     console.log(`Total : ${result}`);
+// }
+// else if(operator.operator === '**'){
+//     result = input1.num1 ** input2.num2;
+//     console.log(`Total : ${result}`);   
+// }
+// else if(operator.operator === '%'){
+//     result = input1.num1 % input2.num2;
+//     console.log(`Total : ${result}`);
+// }
+
+
+
+
+// import inquirer from "inquirer";
+
+// const input1 = await inquirer.prompt({
+//     name : "num1",
+//     type : "number",
+//     message : "Enter the number"
+// })
+
+// const input2 = await inquirer.prompt({
+//     name : "num2",
+//     type : "number",
+//     message : "Enter the second number"
+// })
+
+
+// if (input1.num1 > input2.num2){
+//     console.log(`${input1.num1} is greater then ${input2.num2}`);
+// }
+// else if(input1.num1 < input2.num2){
+//     console.log(`${input1.num1} is less then ${input2.num2}`);
+// }
+// else if(input1.num1 === input2.num2){
+//     console.log(`${input1.num1} is less then ${input2.num2}`);    
+// }
+// else("PLease enter the correct formate")
+
+
+
+
+
+
+
+
---
+++ 05numbersgame.ts
@@ -0,0 +1,53 @@
+// import inquirer from "inquirer";
+
+// const input1 = await inquirer.prompt({
+//     name : "num1",
+//     type : "number",
+//     message : "Enter the number"
+// })
+
+// const input2 = await inquirer.prompt({
+//     name : "num2",
+//     type : "number",
+//     message : "Enter the second number"
+// })
+
+
+// if (input1.num1 > input2.num2){
+//     console.log(`${input1.num1} is greater then ${input2.num2}`);
+// }
+// else if(input1.num1 < input2.num2){
+//     console.log(`${input1.num1} is less then ${input2.num2}`);
+// }
+// else if(input1.num1 === input2.num2){
+//     console.log(`${input1.num1} is equal too ${input2.num2}`);    
+// }
+// else("PLease enter the correct formate")
+
+
+// import inquirer from "inquirer";
+
+// const input1 = await inquirer.prompt({
+//     name : "num1",
+//     type : "number",
+//     message : "Enter the number"
+// })
+
+// const number = 5;
+
+
+// if(input1.num1 === number){
+//     console.log(`You have guess the right number ${number}`)
+// }
+// else
+// {console.log("Please try agin")};
+
+
+
+
+
+
+
+
+
+
